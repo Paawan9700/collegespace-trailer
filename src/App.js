aaf8aaa6bde9785782f582af77c2de1bae7024db
Paawan Singal <paawansingal09@gmail.com>
2022-04-28 19:43:59 +0530

    Creating Navbar and Routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+import { Navbar } from './components/Navbar';
+import { Home } from './components/Home';
+import { About } from './components/About';
 
 function App() {
   return (
     <div className="App">
-      <h1>this is college space</h1>
+      <Router>
+        <Navbar />
+        <div className="container">
+          <Routes>
+            <Route exact path='/' element={<Home />} />
+            <Route exact path='/about' element={<About />} />
+            {/* <Route exact path='/login' element={<Login />} />
+            <Route exact path='/signup' element={<Signup />} /> */}
+          </Routes>
+        </div>
+      </Router>
+
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+
+export const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+                <Link className="navbar-brand" to="/">College Space</Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/about">About</Link>
+                        </li>
+                        <li className="nav-item dropdown">
+                            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Dropdown
+                            </a>
+                            <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a className="dropdown-item" href="#">Previous Years Papers</a></li>
+                                <li><a className="dropdown-item" href="#">Another action</a></li>
+                                <li><hr className="dropdown-divider" /></li>
+                                <li><a className="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <form className="d-flex">
+                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                        <button className="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+    )
+}
