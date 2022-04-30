b4cb0b9df5b0c24add68fbe7e2341087c64a411f
Paawan Singal <paawansingal09@gmail.com>
2022-04-30 16:31:12 +0530

    Fetching events from context

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import EventContext from '../context/events/EventContext'
 
 export const Home = () => {
+    const context = useContext(EventContext);
+
+    const {events, setevents} = context;
     return (
         <div>
-            <h1>this is college space</h1>
+            <div className="container">
+                <h1>Add New Events</h1>
+
+                <form className='my-3'>
+                    <div className="mb-3">
+                        <label for="exampleInputEmail1" className="form-label">Title</label>
+                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                    </div>
+                    <div className="mb-3">
+                        <label for="exampleInputPassword1" className="form-label">Description</label>
+                        <input type="password" className="form-control" id="exampleInputPassword1" />
+                    </div>
+                    <div className="mb-3 form-check">
+                        <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+                    </div>
+                    <button type="submit" className="btn btn-primary">Add Event</button>
+                </form>
+            </div>
+
+            <div className="container my-3">
+                <h1>All Events</h1>
+                {events.map((event) => {
+                    return event.title;
+                })}
+            </div>
         </div>
     )
 }
--- src/context/events/EventState.js
+++ src/context/events/EventState.js
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, {useState} from 'react';
 import EventContext from './EventContext';
 
 const EventState = (props) => {
 
+    const initialEvents = [
+        {
+          "_id": "62640de9cf15748e9c1a80fd",
+          "title": "gusto night",
+          "description": "drama and all",
+          "tag": "fun event",
+          "Date": "2022-04-23T14:32:09.103Z",
+          "__v": 0
+        },
+        {
+          "_id": "62640e4ccf15748e9c1a80ff",
+          "title": "dj night",
+          "description": "a night full of music and dance",
+          "tag": "fun event",
+          "Date": "2022-04-23T14:33:48.542Z",
+          "__v": 0
+        },
+        {
+          "_id": "62640e6104241723042d0e7e",
+          "title": "dj night",
+          "description": "a night full of music and dance",
+          "tag": "fun event",
+          "Date": "2022-04-23T14:34:09.425Z",
+          "__v": 0
+        },
+        {
+          "_id": "6264342378249b7cd07e9b5b",
+          "title": "gusto night 2",
+          "description": "drama and all 2",
+          "tag": "fun event 2",
+          "Date": "2022-04-23T17:15:15.837Z",
+          "__v": 0
+        }
+      ]
+
+    const [events, setevents] = useState(initialEvents)
+
     return (
         // context api basic syntax
-        <EventContext.Provider value={{}}>
+        <EventContext.Provider value={{events, setevents}}>
             {props.children}
         </EventContext.Provider>
     )
